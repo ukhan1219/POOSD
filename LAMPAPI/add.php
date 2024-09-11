53da7b56e6af138bb161c3c5515d3b591319c739
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2024-09-11 17:14:17 -0400

    add.php:

--- LAMPAPI/add.php
+++ LAMPAPI/add.php
@@ -12,17 +12,16 @@ if ($conn->connect_error) {
     returnWithError($conn->connect_error);
 } else {
     $stmt = $conn->prepare("SELECT ID FROM contacts WHERE (email = ? OR phone = ?) AND userID = ?");
-    $stmt->bind_param("sss", $email, $phone, $userID);
+    $stmt->bind_param("ssi", $email, $phone, $userID);
     $stmt->execute();
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        $response = array("error" => "Contact already exists.");
-    echo json_encode($response);
+        returnWithError("A contact with this email or phone already exists.");
     } else {
         $stmt->close();
         $stmt = $conn->prepare("INSERT INTO contacts (name, email, phone, userID) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $name, $email, $phone, $userID);
+        $stmt->bind_param("sssi", $name, $email, $phone, $userID);
         $stmt->execute();
         returnWithError("");
     }
