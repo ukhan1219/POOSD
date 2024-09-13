4bb650570e0121126c859abc63a221138e64f376
Usman Khan <usman.k1219@gmail.com>
2024-09-13 08:52:33 -0400

    edit

--- js/code.js
+++ js/code.js
@@ -66,8 +66,8 @@ function doSearch() {
 //  returning all contacts
 function enableEditMode(contactID) {
     let row = document.querySelector(`tr[data-id="${contactID}"]`)
-    row.querySelectorAll('span').forEach(el => el.classList.add('hidden'));
-    row.querySelectorAll('input').forEach(el => el.classList.remove('hidden'));
+    row.querySelectorAll('span').forEach(row => row.classList.add('hidden'));
+    row.querySelectorAll('input').forEach(row => row.classList.remove('hidden'));
     row.querySelector('.edit-btn').classList.add('hidden');
     row.querySelector('.save-btn').classList.remove('hidden');
     row.querySelector('.cancel-btn').classList.remove('hidden');
@@ -77,8 +77,8 @@ function enableEditMode(contactID) {
 //  function to cancel changes of the contact incase the user want to keep the contact as is 
 function cancelEdit(contactID) {
     let row = document.querySelector(`tr[data-id="${contactID}"]`);
-    row.querySelectorAll('span').forEach(el => el.classList.remove('hidden'));
-    row.querySelectorAll('input').forEach(el => el.classList.add('hidden'));
+    row.querySelectorAll('span').forEach(row => row.classList.remove('hidden'));
+    row.querySelectorAll('input').forEach(row => row.classList.add('hidden'));
     row.querySelector('.edit-btn').classList.remove('hidden');
     row.querySelector('.save-btn').classList.add('hidden');
     row.querySelector('.cancel-btn').classList.add('hidden');
@@ -110,7 +110,13 @@ function saveContact(contactID) {
                 row.querySelector('.contact-email').textContent = email;
                 row.querySelector('.contact-phone').textContent = phone;
 
-                document.getElementById("contactEditResult").innerHTML = "";
+                row.querySelectorAll('span').forEach(row => row.classList.remove('hidden'));
+                row.querySelectorAll('input').forEach(row => row.classList.add('hidden'));
+                row.querySelector('.edit-btn').classList.remove('hidden');
+                row.querySelector('.save-btn').classList.add('hidden');
+                row.querySelector('cancel-btn').classList.add('hidden');
+
+                document.getElementById("contactEditResult").innerHTML = "Contact has been Edited successfully!";
             }
         };
         xhr.send(payload);
